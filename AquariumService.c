b11a96230b3e07d2c71dc19c5029b10a3193fc37
Casper Bladt <casper.bladt@bitzerdk.com>
2021-10-09 06:45:11 +0200

    added actual heat transfer calcs

--- AquariumService.c
+++ AquariumService.c
@@ -1,153 +1,151 @@
 #include <AquariumService.h>
 #include "math.h"
-#include "stdio.h"
 
 static Boolean_t NowIsBetweenTimestamps(AquariumServiceContext_t* context, Hour startHour, Minute startMinute, Hour stopHour, Minute stopMinute)
 {
-    Timestamp_t now;
-    context->Time.getTime(255, 255, &now);
+  Timestamp_t now;
+  context->Time.getTime(255, 255, &now);
 
-    Timestamp_t start;
-    context->Time.getTime(startHour, startMinute, &start);
+  Timestamp_t start;
+  context->Time.getTime(startHour, startMinute, &start);
 
-    Timestamp_t stop;
-    context->Time.getTime(stopHour, stopMinute, &stop);
+  Timestamp_t stop;
+  context->Time.getTime(stopHour, stopMinute, &stop);
 
-    return (now >= start) && (now <= stop);
+  return (now >= start) && (now <= stop);
 }
 
 static Boolean_t TemperatureOk(Temperature_t t)
 {
-	return (t > -50) && (t < 100);
-}
+  return (t > -50) && (t < 100);
+  }
 
 static Boolean_t HeaterTempSignalsOk(AquariumServiceContext_t* context)
 {
-	Boolean_t ok = TemperatureOk(context->Input.waterT1);
-	ok &= TemperatureOk(context->Input.waterT2);
-	ok &= TemperatureOk(context->Input.waterTHeat);
+  Boolean_t ok = TemperatureOk(context->Input.waterT1);
+  ok &= TemperatureOk(context->Input.waterT2);
+  ok &= TemperatureOk(context->Input.waterTHeat);
 
-	return ok;
+  return ok;
 }
 
 static Boolean_t HeaterNeeded(AquariumServiceContext_t* context, Temperature_t tWater)
 {
-	float max = context->Parameter.waterTSetpoint + context->Parameter.waterTHysteresis;
-	float min = context->Parameter.waterTSetpoint - context->Parameter.waterTHysteresis;
+  float max = context->Parameter.waterTSetpoint + context->Parameter.waterTHysteresis;
+  float min = context->Parameter.waterTSetpoint - context->Parameter.waterTHysteresis;
 
-	// Stop if tWater is too high.
-	if (tWater > max)
-	{
-	  return 0;
-	}
+  // Stop if tWater is too high.
+  if (tWater > max)
+  {
+    return 0;
+  }
 
-	// Start if tWater is too low.
-	else if (tWater < min)
-	{
-	  return 1;
-	}
+  // Start if tWater is too low.
+  else if (tWater < min)
+  {
+    return 1;
+  }
 
-	// Otherwise just keep going.
-	else
-	{
-	  return context->Output.heaterIsRunning;
-	}
+  // Otherwise just keep going.
+  else
+  {
+    return context->Output.heaterIsRunning;
+  }
 }
 
 static Boolean_t HeaterAllowed(AquariumServiceContext_t* context, Temperature_t tDiff)
 {
-	float max = context->Parameter.heaterTDiffMax + context->Parameter.heaterTDiffHysteresis;
-	float min = context->Parameter.heaterTDiffMax - context->Parameter.heaterTDiffHysteresis;
+  float max = context->Parameter.heaterTDiffMax + context->Parameter.heaterTDiffHysteresis;
+  float min = context->Parameter.heaterTDiffMax - context->Parameter.heaterTDiffHysteresis;
 
-	// Stop if tDiff is too high.
-	if (tDiff > max)
-	{
-	  return 0;
-	}
+  // Stop if tDiff is too high.
+  if (tDiff > max)
+  {
+    return 0;
+  }
 
-	// Start if tDiff is too low.
-	else if (tDiff < min)
-	{
-	  return 1;
-	}
+  // Start if tDiff is too low.
+  else if (tDiff < min)
+  {
+    return 1;
+  }
 
-	// Otherwise just keep going.
-	else
-	{
-	  return context->Output.heaterIsRunning;
-	}
-
+  // Otherwise just keep going.
+  else
+  {
+    return context->Output.heaterIsRunning;
+  }
 }
 
 static void HeaterService(AquariumServiceContext_t* context)
 {
-	Temperature_t tWater = (context->Input.waterT1 + context->Input.waterT2) / 2;
-	Temperature_t tDiff = context->Input.waterTHeat - tWater;
+  Temperature_t tWater = (context->Input.waterT1 + context->Input.waterT2) / 2;
+  Temperature_t tDiff = context->Input.waterTHeat - tWater;
 
-	Boolean_t run = context->Parameter.enabled;
-	run &= HeaterTempSignalsOk(context);
-	run &= HeaterNeeded(context, tWater);
-	run &= HeaterAllowed(context, tDiff);
+  Boolean_t run = context->Parameter.enabled;
+  run &= HeaterTempSignalsOk(context);
+  run &= HeaterNeeded(context, tWater);
+  run &= HeaterAllowed(context, tDiff);
 
-	if (context->Parameter.onlyRunHeaterAlongWithWaterPump)
-	{
-	  run &= context->Output.waterPumpIsRunning;
-	}
+  if (context->Parameter.onlyRunHeaterAlongWithWaterPump)
+  {
+    run &= context->Output.waterPumpIsRunning;
+  }
 
-	context->Output.heaterIsRunning = run;
+  context->Output.heaterIsRunning = run;
 }
 
 static void WaterPumpService(AquariumServiceContext_t* context)
 {
-    if (NowIsBetweenTimestamps(context, context->Parameter.waterPumpBeginHour, context->Parameter.waterPumpBeginMinute, context->Parameter.waterPumpStopHour, context->Parameter.waterPumpStopMinute))
-    {
-        context->Output.waterPumpIsRunning = 1;
-    }
-    else
-    {
-    	context->Output.waterPumpIsRunning = 0;
-    }
+  if (NowIsBetweenTimestamps(context, context->Parameter.waterPumpBeginHour, context->Parameter.waterPumpBeginMinute, context->Parameter.waterPumpStopHour, context->Parameter.waterPumpStopMinute))
+  {
+      context->Output.waterPumpIsRunning = 1;
+  }
+  else
+  {
+      context->Output.waterPumpIsRunning = 0;
+  }
 
-    context->Output.waterPumpIsRunning |= context->Input.externStartSignal;
-    context->Output.waterPumpIsRunning &= context->Parameter.enabled;
+  context->Output.waterPumpIsRunning |= context->Input.externStartSignal;
+  context->Output.waterPumpIsRunning &= context->Parameter.enabled;
 }
 
 static void AirPumpService(AquariumServiceContext_t* context)
 {
-    if (NowIsBetweenTimestamps(context, context->Parameter.airPumpBeginHour, context->Parameter.airPumpBeginMinute, context->Parameter.airPumpStopHour, context->Parameter.airPumpStopHour))
-    {
-    	context->Output.airPumpIsRunning = 1;
-    }
-    else
-    {
-    	context->Output.airPumpIsRunning = 0;
-    }
+  if (NowIsBetweenTimestamps(context, context->Parameter.airPumpBeginHour, context->Parameter.airPumpBeginMinute, context->Parameter.airPumpStopHour, context->Parameter.airPumpStopHour))
+  {
+      context->Output.airPumpIsRunning = 1;
+  }
+  else
+  {
+      context->Output.airPumpIsRunning = 0;
+  }
 
-    context->Output.airPumpIsRunning &= context->Parameter.enabled;
+  context->Output.airPumpIsRunning &= context->Parameter.enabled;
 }
 
 static void LightService(AquariumServiceContext_t* context)
 {
-    if (NowIsBetweenTimestamps(context, context->Parameter.lightBeginHour, context->Parameter.lightBeginMinute, context->Parameter.lightStopHour, context->Parameter.lightStopMinute))
-    {
-    	context->Output.lightIsRunning = 1;
-    }
-    else
-    {
-    	context->Output.lightIsRunning = 0;
-    }
+  if (NowIsBetweenTimestamps(context, context->Parameter.lightBeginHour, context->Parameter.lightBeginMinute, context->Parameter.lightStopHour, context->Parameter.lightStopMinute))
+  {
+      context->Output.lightIsRunning = 1;
+  }
+  else
+  {
+      context->Output.lightIsRunning = 0;
+  }
 
-    context->Output.lightIsRunning |= context->Input.externStartSignal;
-    context->Output.lightIsRunning &= context->Parameter.enabled;
+  context->Output.lightIsRunning |= context->Input.externStartSignal;
+  context->Output.lightIsRunning &= context->Parameter.enabled;
 }
 
 void AquariumService_Service(AquariumServiceContext_t* context)
 {
-    HeaterService(context);
+  HeaterService(context);
 
-    WaterPumpService(context);
+  WaterPumpService(context);
 
-    AirPumpService(context);
+  AirPumpService(context);
 
-    LightService(context);
+  LightService(context);
 }
--- main.cpp
+++ main.cpp
@@ -1,75 +1,111 @@
 #include <iostream>
+#include <iomanip>
 #include <time.h>
 #include <thread>
 
 #include "AquariumService.h"
 
-#define Print(exp) (std::cout << #exp << "\t" << std::to_string(exp) << std::endl)
+#define  Print(exp) (std::cout << #exp << "\t" << std::to_string(exp) << std::endl)
+#define fPrint(exp) (std::cout << #exp << "\t\t" << std::fixed << std::setprecision(2) << exp << std::endl)
 
-static const constexpr auto TickMs = 100;
-static const constexpr auto HeatGain = 1.001;
-static const constexpr auto HeatDiffGain = 1.002;
-static const constexpr auto TimeGain = 5;
-static const constexpr auto RoomTemp = 21;
+static const constexpr auto TickMs    = 100;
+static const constexpr auto TimeStep  = 1;
+static const constexpr auto Positive  = 1.002f;
+static const constexpr auto Negative  = 0.998f;
+static const constexpr auto RoomTemp  = 21.0f;
 
-Temperature_t _waterTemp;
-Temperature_t _heaterTemp;
+static const constexpr auto AquariumLengthM = 0.5f;
+static const constexpr auto AquariumWidthM = 0.2f;
+static const constexpr auto AquariumHeightM = 0.3f;
+static const constexpr auto AquariumVolumeL = AquariumWidthM * AquariumLengthM * AquariumHeightM * 1000;
+static const constexpr auto AquariumHeaterEffect = 400;
 
-unsigned short _currentYear;
-unsigned char _currentMonth;
-unsigned char _currentDay;
-Hour _currentHour;
-Minute _currentMinute;
+static const constexpr auto WaterSpecificHeat = 4186;
 
-Boolean_t GetExternStartSignal()
+static Temperature_t _waterTemp;
+static Temperature_t _heaterTemp;
+
+static unsigned short _currentYear;
+static unsigned char _currentMonth;
+static unsigned char _currentDay;
+static Hour _currentHour;
+static Minute _currentMinute;
+
+static unsigned char _counterLastHeatingState;
+static unsigned long _counterHeatingTime;
+static unsigned long _counterHeatingSwitch;
+static unsigned long _counterRunTime;
+
+template <typename T>
+static T Limit(T in, T min, T max)
 {
-    if (_currentHour == 2 || _currentHour == 3 || _currentHour == 4)
-    {
-      return 1;
-    }
-    else
-    {
-      return 0;
-    }
+  if (in < min)
+  {
+    return min;
+  }
+
+  if (in > max)
+  {
+    return max;
+  }
+
+  return in;
+}
+
+static auto SinusLikeFluctuation()
+{
+  return (_currentMinute-30.0f)/10000.0f;
+}
+
+static Boolean_t GetExternStartSignal()
+{
+  if (_currentHour == 2 || _currentHour == 3 || _currentHour == 4)
+  {
+    return 1;
+  }
+  else
+  {
+    return 0;
+  }
 }
 
 static void GetTime(Hour hour, Minute minute, Timestamp_t* timestamp)
 {
-    if (hour == 255 && minute == 255)
-    {
-        struct tm info;
-        info.tm_sec = 0;
-        info.tm_min = _currentMinute;
-        info.tm_hour = _currentHour;
-        info.tm_mday = _currentDay;
-        info.tm_mon = _currentMonth-1;
-        info.tm_year = _currentYear-1900;
-        *timestamp = mktime(&info);
-    }
-    else
-    {
-        struct tm info;
-        info.tm_sec = 0;
-        info.tm_min = minute;
-        info.tm_hour = hour;
-        info.tm_mday = _currentDay;
-        info.tm_mon = _currentMonth-1;
-        info.tm_year = _currentYear-1900;
-        *timestamp = mktime(&info);
-    }    
+  if (hour == 255 && minute == 255)
+  {
+    struct tm info;
+    info.tm_sec = 0;
+    info.tm_min = _currentMinute;
+    info.tm_hour = _currentHour;
+    info.tm_mday = _currentDay;
+    info.tm_mon = _currentMonth-1;
+    info.tm_year = _currentYear-1900;
+    *timestamp = mktime(&info);
+  }
+  else
+  {
+    struct tm info;
+    info.tm_sec = 0;
+    info.tm_min = minute;
+    info.tm_hour = hour;
+    info.tm_mday = _currentDay;
+    info.tm_mon = _currentMonth-1;
+    info.tm_year = _currentYear-1900;
+    *timestamp = mktime(&info);
+  }
 }
 
-void RewriteScreen(AquariumServiceContext_t& ctx)
+static void RewriteScreen(AquariumServiceContext_t& ctx)
 {
   std::cout << "Time" << std::endl;
-  std::cout << "Hour\t" << std::to_string(_currentHour) << std::endl;
-  std::cout << "Minute\t" << std::to_string(_currentMinute) << std::endl;
+  std::cout << "Hour\t\t\t\t" << std::to_string(_currentHour) << std::endl;
+  std::cout << "Minute\t\t\t\t" << std::to_string(_currentMinute) << std::endl;
   std::cout << std::endl;
 
   std::cout << "Temperatures" << std::endl;
-  Print(ctx.Input.waterT1);
-  Print(ctx.Input.waterT2);
-  Print(ctx.Input.waterTHeat);
+  fPrint(ctx.Input.waterT1);
+  fPrint(ctx.Input.waterT2);
+  fPrint(ctx.Input.waterTHeat);
   Print(ctx.Input.externStartSignal);
   std::cout << std::endl;
 
@@ -79,72 +115,92 @@ void RewriteScreen(AquariumServiceContext_t& ctx)
   Print(ctx.Output.heaterIsRunning);
   Print(ctx.Output.lightIsRunning);
   std::cout << std::endl;
+
+  std::cout << "Counters" << std::endl;
+  std::cout << "Runtime Hours\t\t\t" << std::to_string(_counterRunTime / 60) << std::endl;
+  std::cout << "HeatingTime Minutes\t\t" << std::to_string(_counterHeatingTime) << std::endl;
+  std::cout << "HeatingTime OnOff\t\t" << std::to_string(_counterHeatingSwitch) << std::endl;
+  std::cout << std::endl;
 }
 
-void SimulateWater(AquariumServiceContext_t& context)
+static void SimulateWater(AquariumServiceContext_t& context)
 {
-    // Actively heating the water.
-    if (context.Output.heaterIsRunning)
+  // Actively heating the water.
+  if (context.Output.heaterIsRunning)
+  {
+    auto tWater = (_waterTemp + _heaterTemp) / 2;
+    auto tTarget = context.Parameter.waterTSetpoint + context.Parameter.waterTHysteresis;
+    auto totalMinutes = AquariumVolumeL * WaterSpecificHeat * (tTarget - tWater) / AquariumHeaterEffect / 60;
+    auto step = TimeStep / totalMinutes;
+
+    _waterTemp  += step * Negative;
+    _heaterTemp += step * Positive;
+  }
+
+  // Heater is off, but element still warmer than the water.
+  else if (_heaterTemp > _waterTemp)
+  {
+    _waterTemp  *= Positive;
+    _heaterTemp *= Negative;
+}
+
+  // Nominal heat dissipation.
+  else
+  {
+    // Calculate actual dissipation.
+    auto tWater = (_waterTemp + _heaterTemp) / 2;
+    if (tWater > RoomTemp)
     {
-        _waterTemp *= HeatGain;
-        _heaterTemp *= HeatDiffGain;
+      auto powerW = 5.68f / (tWater * (AquariumWidthM * AquariumLengthM));
+      auto totalMinutes = AquariumVolumeL * WaterSpecificHeat * (RoomTemp - tWater) / powerW / 60;
+      auto step = TimeStep / totalMinutes;
+
+      _waterTemp  += step;
+      _heaterTemp += step;
     }
 
-    // Heater is off, but element still warmer than the water.
-    else if (_heaterTemp > _waterTemp)
-    {
-        _waterTemp *= HeatGain;
-        _heaterTemp *= 0.99f;
-    }
-
-    // Nominal heat dissipation.
     else
     {
-        _waterTemp /= HeatGain;
-        _heaterTemp /= HeatDiffGain;
-
-        if (_waterTemp < RoomTemp)
-        {
-            _waterTemp = RoomTemp;
-        }
-
-        if (_heaterTemp < _waterTemp)
-        {
-            _heaterTemp = _waterTemp;
-        }
+      _waterTemp  += SinusLikeFluctuation();
+      _heaterTemp += SinusLikeFluctuation();
     }
+
+    // Limit results.
+    _waterTemp  = Limit<decltype(_waterTemp)> (_waterTemp,  RoomTemp*0.8, RoomTemp*1.2);
+    _heaterTemp = Limit<decltype(_heaterTemp)>(_heaterTemp, RoomTemp*0.8, RoomTemp*1.2);
+  }
 }
 
-void SimulateTime()
+static void SimulateTime()
 {
-    _currentMinute += (1 * TimeGain);
+  _currentMinute += TimeStep;
 
-    if (_currentMinute > 59)
-    {
-        _currentHour += 1;
-        _currentMinute = 0;
-    }
+  if (_currentMinute > 59)
+  {
+    _currentHour += 1;
+    _currentMinute = 0;
+  }
 
-    if (_currentHour > 23)
-    {
-        _currentDay += 1;
-        _currentHour = 0;
-    }
+  if (_currentHour > 23)
+  {
+    _currentDay += 1;
+    _currentHour = 0;
+  }
 
-    if (_currentDay > 30)
-    {
-        _currentMonth += 1;
-        _currentDay = 0;
-    }
+  if (_currentDay > 30)
+  {
+    _currentMonth += 1;
+    _currentDay = 0;
+  }
 
-    if (_currentMonth > 12)
-    {
-        _currentYear += 1;
-        _currentMonth = 0;
-    }
+  if (_currentMonth > 12)
+  {
+    _currentYear += 1;
+    _currentMonth = 0;
+  }
 }
 
-void PrepareAquariumService(AquariumServiceContext_t& context)
+static void PrepareAquariumService(AquariumServiceContext_t& context)
 {
   context.Input.waterT1 = (_waterTemp);
   context.Input.waterT2 = (_waterTemp - 0.356f);
@@ -179,6 +235,22 @@ void PrepareAquariumService(AquariumServiceContext_t& context)
   context.Time.currentSecond = 0;
 }
 
+static void ServiceCounters(AquariumServiceContext_t& context)
+{
+  if (context.Output.heaterIsRunning)
+  {
+    _counterHeatingTime += TimeStep;
+  }
+
+  if (context.Output.heaterIsRunning != _counterLastHeatingState)
+  {
+    _counterHeatingSwitch++;
+    _counterLastHeatingState = context.Output.heaterIsRunning;
+  }
+
+  _counterRunTime += TimeStep;
+}
+
 int main()
 {
     AquariumServiceContext_t context;
@@ -189,22 +261,29 @@ int main()
     _currentYear = 2021;
     _currentMonth = 10;
     _currentDay = 8;
-    _currentHour = 0;
+    _currentHour = 5;
     _currentMinute = 0;
+
+    _counterLastHeatingState = 0;
+    _counterHeatingTime = 0;
+    _counterHeatingSwitch = 0;
+    _counterRunTime = 0;
 
     PrepareAquariumService(context);
 
     while (true)
     {
-        SimulateWater(context);
+      SimulateWater(context);
 
-        SimulateTime();
+      SimulateTime();
 
-        PrepareAquariumService(context);
-        AquariumService_Service(&context);
+      PrepareAquariumService(context);
+      AquariumService_Service(&context);
 
-        RewriteScreen(context);
+      RewriteScreen(context);
 
-        std::this_thread::sleep_for(std::chrono::milliseconds(TickMs));
+      ServiceCounters(context);
+
+      std::this_thread::sleep_for(std::chrono::milliseconds(TickMs));
     }
 }
